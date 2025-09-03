0ae9e95a9fd3526d5a452baddc02431623803275
samanthacrys <107442367+samanthacrys@users.noreply.github.com>
2025-09-03 10:31:53 -0700

    First Commit
    Adding the Crochet World Project to the Git Hub repository.

---
+++ CrochetPatternV1/CrochetPatternV1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CrochetPatternV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.CrochetWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnClickThis.Location = new System.Drawing.Point(315, 206);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(184, 46);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Click This";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
+            // 
+            // CrochetWorld
+            // 
+            this.CrochetWorld.AutoSize = true;
+            this.CrochetWorld.Location = new System.Drawing.Point(293, 252);
+            this.CrochetWorld.Name = "CrochetWorld";
+            this.CrochetWorld.Size = new System.Drawing.Size(0, 25);
+            this.CrochetWorld.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 482);
+            this.Controls.Add(this.CrochetWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label CrochetWorld;
+    }
+}
+
