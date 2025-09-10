2490e24ab4e7fae25644bbfcdfce028628e156cb
samanthacrys <107442367+samanthacrys@users.noreply.github.com>
2025-09-10 12:32:43 -0700

    Added Second page
    Added the base form for the second page of the app.

---
+++ CrochetWorldMAUI/CrochetWorldMAUI/CrochetNote.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace CrochetWorldMAUI
+{
+    public partial class CrochetNote : ContentPage
+    {
+
+        public CrochetNote()
+        {
+            InitializeComponent();
+        }
+
+        private async void Return(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private void OnSubmitButtonClicked(object sender, EventArgs e)
+        {
+            ReadOnlyEntry.Text = CrochetStitches.Text;
+        }
+    }
+
+}
